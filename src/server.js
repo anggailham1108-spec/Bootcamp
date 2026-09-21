2a44919457a2a1b28bf0a68779b92166f0e39cd3
Angga Ilham <angga.ilham1108@gmail.com>
2026-09-21 18:13:08 +0700

    pindahin server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const authRoutes = require('./src/routes/authRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 app.use(express.json()); // supaya req.body bisa dibaca dalam format JSON
 app.use('/', authRoutes);
